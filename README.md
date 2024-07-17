d68cf0f8bc08883332602900e4f044e7029a9176
Samu <samroval@gmail.com>
2024-07-17 02:59:13 -0400

    Se agregan funcionalidades. Reimplementación del código, refactoring.

--- README.md
+++ README.md
@@ -1,18 +1,75 @@
-<h4> Microservicio creado con Node para obtener el balance de la tarjeta Bip!</h4>
-<p> Web Scrapping que se detalla en el archivo lib/TarjetaBip.js</p>
-<hr>
+# red-bip-api
+<h4> Microservicio creado con Node para obtener datos de la red metro en un formato amigable. (Chile) </h4>
+<br>
 
-<h4> 📝 Instrucciones </h4>
-<p> Para ejecutar index.js:  </p>
+<h2> 📝 Prerrequisitos </h2>
+<br>
+    - Tener NodeJS instalado en tu equipo.
+
+<h2> 📝 Instrucciones </h2>
+<br>
+<p> Para ejecutar el servidor de express (index.js:)  </p>
 
 ```
 npm run api
 ```
 
-<p> Para obtener un .json con los datos debe hacer una petición GET a esta ruta: <p>
+<p> Para obtener un .json con los datos de la tarjeta debe hacer una petición GET a esta ruta: <p>
 
 ```
-localhost:3000/api/<numero de la tarjeta bip>
+localhost:3000/api/bip/<numero de tu bip>
 ```
+
+<strong>Ejemplo:</strong>
+
+```
+{
+    "respuestaBip": true,
+    "datosTarjeta": {
+        "numeroTarjeta": "29927157",
+        "tipoContrato": "Contrato Activo",
+        "balance": "$110",
+        "fechaSaldo": "16/07/2023 15:14"
+    }
+}
+```
+
+<p> Para obtener datos del estado del servicio de metro se debe hacer una petición GET a esta ruta: <p>
+
+```
+localhost:3000/api/estadored
+```
+
+<strong>Ejemplo:</strong>
+
+```
+{
+    "respuestaBip": true,
+    "estadoRedMetro": {
+        "L1": "Línea operativa",
+        "L2": "Línea operativa",
+        "L3": "Línea operativa",
+        "L4": "Línea operativa",
+        "L4a": "Línea operativa",
+        "L5": "Línea operativa",
+        "L6": "Línea operativa",
+        "trenNos": "Hay problemas en el servicio"
+    }
+}
+```
+
+<p> Si hay errores, el estado de respuestaBip cambiará a false, y aparecerá la key "error" con la descripción del error:  </p>
+<strong>Ejemplo:</strong>
+
+```
+{
+    "respuestaBip": false,
+    "error": "El parámetro ingresado no cumple con los requisitos para ser aceptada como Bip!."
+}
+```
+
+
+
+
 
 
--- index.js
+++ index.js
@@ -1,39 +1,80 @@
-const express = require('express')
-const tarjetaBip = require('./lib/tarjetaBip')
+const express = require("express");
+const env = require('dotenv').config()
+const tarjetaBip = require("./lib/tarjetaBip");
+const estadoRed = require("./lib/estadoRed");
+let api = express();
 
-let api = express()
-port = 3000
+api.set("port", process.env.PORT)
+api.get("/api/estadored", (req, res) => {
 
-api.get('/api/:numerotarjeta', (req, res) => {
-    numtarjeta = 0
-    if (res.statusCode == 200) {
-      numtarjeta = req.params.numerotarjeta;
-      tarjetaBip(numtarjeta).then((data) => {
-        const jsonData = {
-          numTarjeta: data[0],
-          tipoContrato: data[1],
-          monto: data[2],
-          utlimaVezActualizado: data[3],
-        };
-        res.json(jsonData)
-    })    
-    } else {
-      res.send(`Ocurrió un error: ${res.statusCode}`);
-    }
+  estadoRed()
+    .then((data) => {
+      jsonData = {
+        respuestaBip: true,
+        estadoRedMetro: {
+          L1: data[0],
+          L2: data[1],
+          L3: data[2],
+          L4: data[3],
+          L4a: data[4],
+          L5: data[5],
+          L6: data[6],
+          trenNos: data[7],
+        },
+      };
+      res.send(jsonData);
+    })
+    .catch((error) => {
+      jsonData = {
+        respuestaBip: false,
+        error,
+      };
+      res.send(jsonData);
+    });
+});
+
+api.get("/api/bip/:numerotarjeta", (req, res) => {
+  let jsonData = {};
+  numtarjeta = req.params.numerotarjeta;
+  tarjetaBip(numtarjeta)
+    .then((data) => {
+      jsonData = {
+        respuestaBip: true,
+        datosTarjeta: {
+          numeroTarjeta: data[1],
+          tipoContrato: data[2],
+          balance: data[3],
+          fechaSaldo: data[4],
+        },
+      };
+
+      res.json(jsonData);
+    })
+    .catch((error) => {
+      jsonData = {
+        respuestaBip: false,
+        error,
+      }
+      res.json(jsonData)
+    });
+});
+
+api.all("/api/bip/", (req, res, next) => {
+  res.redirect("/")
+});
+
+
+api.all("/api/", (req, res, next) => {
+  res.redirect("/");
+});
+
+api.all("/", (req, res) => {
+  res.json({respuestaBip: "Método no permitido."});
+});
+api.all("*", (req,res) => {
+  res.json({respuestaBip: "Método no permitido."})
 })
 
-api.all('/api', (req, res, next) => {
-  res.redirect('/')
-})
-
-api.all('/', (req, res) => {
-  const jsonData = {
-    'error' : 'Método no permitido'
-  }
-  res.json(jsonData)
-})
-
-api.listen(port, () => {
-    console.log('La aplicación de express está escuchando')
-})
-
+api.listen(api.get("port"), () => {
+  console.log(`La aplicación de express está a la escucha en el puerto ${api.get("port")}`);
+});
